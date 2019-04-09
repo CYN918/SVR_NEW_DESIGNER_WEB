1371434790037875273dda13586e56ac35b8766f
470980592@qq.com <470980592@qq.com>
2019-04-09 14:58:01 +0800

    2019-04-09-03

--- src/views/upload/upload.vue
+++ src/views/upload/upload.vue
@@ -63,13 +63,7 @@
 				<div class="page2_2_1">
 					<div class="page2_2_1_1">作品标签<span>标签可以将作品自动推荐给可能感兴趣的人</span></div>
 					<div class="page2_2_1_2">
-<<<<<<< HEAD
-						<div><Input class="userBoxd2" v-model="tags" :keyup="keydown"  :oType="'max'" :max="10"   :type="'text'" :placeholder="'输入标签，回车添加标签'" ref="tageds"></Input>还可添加{{form.labels.length}}个标签</div>
-=======
-						<div><Input class="userBoxd2" v-model="tags" :keyup="keydown"  :oType="'max'" :max="10"   :type="'text'" :placeholder="'输入标签，回车添加标签'" ref="tageds"></Input>	还可添加2个标签</div>
->>>>>>> e2bca0f162055aa7c90198910725e83ce485daa0
-						
-						
+						<div><Input class="userBoxd2" v-model="tags" :keyup="keydown"  :oType="'max'" :max="10"   :type="'text'" :placeholder="'输入标签，回车添加标签'" ref="tageds"></Input>还可添加{{5-form.labels.length}}个标签</div>
 						<div class="page2_2_1_2x">
 							<span v-for="(el,index) in form.labels" :key="index">{{el}}<span @click="deletTage(index)" class="iconfont pend">&#xe619;</span></span>
 						</div>
