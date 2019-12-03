8315e9314f3ffe2cec94c9bc58636ed1acdae2b3
sanquan <470980592@qq.com>
2019-12-03 15:46:02 +0800

    2019-12-3

--- src/pages/index/views/personalCenter/info.vue
+++ src/pages/index/views/personalCenter/info.vue
@@ -17,8 +17,9 @@
 						<span>职业</span><span>{{userInfo.vocation}}</span>
 					</div>
 					<div v-if="userInfo.education_school">
-						<span class="forddf">学校</span><span>{{userInfo.education_school}}</span>
+						<span>学校</span><span>{{userInfo.education_school}}</span>
 					</div>
+
 					<div>
 						<span>所在地</span><span>{{userInfo.province+' '+userInfo.city}}</span>
 					</div>
