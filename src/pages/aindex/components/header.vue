49005e3c3917f18fabfd1e6cfda09d0025895a98
sanquan <470980592@qq.com>
2020-05-11 15:43:37 +0800

    20200511

--- src/pages/aindex/components/header.vue
+++ src/pages/aindex/components/header.vue
@@ -1,9 +1,9 @@
 <template>
 	<header class="header">
 		<img @click="goIndex" class="log" src="https://static.zookingsoft.com/SVR_NEW_DESIGNER_WEB/New/imge/new/header/logo.svg" alt="">		
-		<span @click="qh()">切换</span>
-		<span @click="shar" class="fxbtn">分享</span>
 		
+		<span v-if="['/activvity','/index'].indexOf($route.path)==-1" @click="shar" class="fxbtn">分享</span>
+		<span v-else @click="qh()" class="qh_pc"><img src="/imge/app/icon_web_h5.svg"/>电脑端</span>
 		<component v-bind:is="tanc.zj" v-model="tanc" ref="tanbox"></component>
 	</header>
 </template>
@@ -18,7 +18,14 @@ export default {
 			tanc:{}
 		}		
 	},
+	mounted: function () {
+		this.init()
+	}, 
+	
 	methods:{
+		init(){
+			console.log(this.$route);
+		},
 		goIndex(){
 			this.$router.push({path: '/index'});
 		},
@@ -57,5 +64,20 @@ export default {
     line-height: 2rem;
     font-size: .6rem;
     right: .9rem;
+}
+.qh_pc{
+	position: absolute;
+    right: 1rem;
+    top: 0;
+    font-size: .8rem;
+    font-family: PingFangSC-Regular,PingFang SC;
+    font-weight: 400;
+    color: rgba(40,40,40,1);
+    line-height: 2rem;
+}
+.qh_pc>img{
+	display: inline-block;
+	vertical-align: top;
+	width: 2rem;
 }
 </style>
