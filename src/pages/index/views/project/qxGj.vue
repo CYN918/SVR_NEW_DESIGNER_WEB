5a21c74b13b533636692a45d3e22669b00701d39
fengshiqiang <oncwnuCJgixCk3btveu6mL9-rp0A@git.weixin.qq.com>
2020-06-15 19:16:36 +0800

    bug

--- src/pages/index/views/project/qxGj.vue
+++ src/pages/index/views/project/qxGj.vue
@@ -29,13 +29,13 @@ export default {
 		},
 		pr_revokeDelivery(){
 			
-			if(this.$parent.$parent.getstate){
-				this.bdtj(this.$parent.$parent.pagename,this.$parent.$parent.getstate(),'[确定]撤回稿件')
-			} 
+			// if(this.$parent.$parent.getstate){
+			// 	this.bdtj(this.$parent.$parent.pagename,this.$parent.$parent.getstate(),'[确定]撤回稿件')
+			// } 
 			
-			if(this.$parent.getstate){
-				this.bdtj(this.$parent.pagename,this.$parent.$parent.getstate(),'[确定]撤回稿件')
-			} 
+			// if(this.$parent.getstate){
+			// 	this.bdtj(this.$parent.pagename,this.$parent.$parent.getstate(),'[确定]撤回稿件')
+			// } 
 			
 			//console.log(this.$parent.pagename,this.$parent.$parent.getstate(),'[确定]撤回稿件')
 			
