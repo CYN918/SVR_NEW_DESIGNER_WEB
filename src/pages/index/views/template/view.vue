c9f23fad6ed7ef1f44c638d257787c241a9d3a01
wukuangyu <964453362@qq.com>
2020-07-04 15:10:28 +0800

    添加页面模板

---
+++ src/pages/index/views/template/view.vue
@@ -0,0 +1,20 @@
+<style src="view.less" scoped></style>
+<template src="view.html"></template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        init() {
+
+        }
+    },
+    created() {
+        this.init()
+    }
+}
+</script>
